e6e20ec8b97df01c75cc054b1a21b39875f014ce
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-02 03:19:13 -0300

    Adicionando arquivo com a resolução do desafio proposto pelo professor Luciano Vasconcelos

---
+++ Aula01/Desafio_KPI.py
@@ -0,0 +1,13 @@
+"""
+DESAFIO
+
+Escreva um programa, em Python, que solicita ao usuário para que digite seu nome, valor do seu salário mensal e o valor do bônus que recebeu. O programa deve, então, imprimir uma mensagem saudando o usuário pelo nome, além de informar o valor do salário em comparação com o bônus recebido.
+"""
+
+nome_usuario = input("Digite o seu nome: ")
+salario_usuario = float(input("Digite o seu salário: "))
+porcentagem_bonus_usuario = float(input("Digite a porcentagem do bônus que você recebeu: "))
+
+kpi_bonus = 1000 + (salario_usuario * porcentagem_bonus_usuario)
+
+print(f"Olá, {nome_usuario}. O seu valor bônus foi de R${kpi_bonus}.")
