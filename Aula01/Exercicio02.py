01740b4363239f1cdfb6729963080b24e83ed127
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-02 01:53:42 -0300

    Adicionando arquivo do Exercício 02

---
+++ Aula01/Exercicio02.py
@@ -0,0 +1,6 @@
+# Crie um programa onde o usuário digite dois valores e apareça a soma.
+
+valor1 = float(input("Digite o primeiro valor: "))
+valor2 = float(input("Digite o segundo valor: "))
+
+print(valor1 + valor2)
