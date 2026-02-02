5cf362d4a9041de41bff8977c30e836d1ab448a8
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-02 02:17:00 -0300

    Alterando arquivo exemplos.py ao incluir novos exemplos utilizados pelo professor Luciano Vasconcelos

--- Aula01/exemplos.py
+++ Aula01/exemplos.py
@@ -8,3 +8,13 @@ print ("Olá" + " " + "Turma")
 input("Digite seu nome: ")
 print(input("Digite seu nome: "))
 print("Olá, " + input("Digite seu nome: ") + "!")
+
+# Tipos de dados
+numero = 3 # int
+print(type(numero))
+numero_decimal = 4.78 # float
+print(type(numero_decimal))
+nome_de_usuario = "Luciano" # string
+print(type(nome_de_usuario))
+verdadeiro = True # bool
+print(type(verdadeiro))
