677ea684e19642a82625d069d0ed1a95be91265d
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-03 02:06:30 -0300

    Adicionando arquivo do Exercício 03

---
+++ Aula02/exercicio03.py
@@ -0,0 +1,6 @@
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+print(f"A multiplicação de {numero1} por {numero2} é igual a {numero1 * numero2}. ")
