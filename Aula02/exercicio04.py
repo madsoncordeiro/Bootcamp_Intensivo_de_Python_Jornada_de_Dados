aa3fc08612526a6180f35d59a57c3e3b59998c30
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-03 02:17:02 -0300

    Adicionando arquivo do Exercício 04

---
+++ Aula02/exercicio04.py
@@ -0,0 +1,6 @@
+# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+numero1 = int(input("Digite o primeiro número inteiro: "))
+numero2 = int(input("Digite o segundo número inteiro: "))
+
+print(f"A divisão inteira de {numero1} por {numero2} é igual a {numero1 // numero2}. ")
