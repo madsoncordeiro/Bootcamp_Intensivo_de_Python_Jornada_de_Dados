650dc0519efe56e495c9206679258a2e982b8dff
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-03 06:37:37 -0300

    Adicionando arquivo do Exercício 06

---
+++ Aula02/exercicio06.py
@@ -0,0 +1,6 @@
+# 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
+
+numero1 = float(input("Digite o primeiro número decimal: "))
+numero2 = float(input("Digite o segundo número decimal: "))
+
+print(f"A soma entre {numero1} e {numero2} é igual a {round(numero1 + numero2, 2)}. ")
