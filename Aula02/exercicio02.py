fd14571eb45f6914d04c37356fd885e688ae6bad
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-03 01:59:29 -0300

    Adicionando arquivo do Exercício 02

---
+++ Aula02/exercicio02.py
@@ -0,0 +1,5 @@
+# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+numero = int(input("Digite um número: "))
+
+print(f"O resto da divisão de {numero} por 5 é igual a {numero % 5}. ")
