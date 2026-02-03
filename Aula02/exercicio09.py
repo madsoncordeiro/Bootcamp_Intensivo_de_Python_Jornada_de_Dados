6a24b5336916c7d79be7f9d9d3a484f7d885913e
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-03 08:19:37 -0300

    Adicionando arquivo do Exercício 09

---
+++ Aula02/exercicio09.py
@@ -0,0 +1,6 @@
+# 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+
+celsius = float(input("Digite a temperatura em graus Celsius: "))
+fahrenheit = float((celsius * 1.8) + 32)
+
+print(f"A temperatura de {round(celsius, 0)}°C corresponde a {round(fahrenheit, 2)}°F. ")
