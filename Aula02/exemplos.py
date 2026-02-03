2b599c24728b4971d9e3e9eb3de807cf53bf8a1e
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-03 01:13:12 -0300

    Adicionando arquivo com exemplos utilizados pelo professor Luciano Vasconcelos em aula, além daqueles exemplos que eu também elaborei nos meus estudos

---
+++ Aula02/exemplos.py
@@ -0,0 +1,21 @@
+nome_aluno = "Gustavo"
+
+print(type(nome_aluno))
+print(type("Gustavo"))
+
+print(nome_aluno.upper())
+print(nome_aluno.lower())
+print(" Gustavo ".strip())
+
+email_aluno = "gustavo@email.com"
+
+print(email_aluno.split("@"))
+
+
+valor_1 = True
+valor_2 = True
+
+print(valor_1 and valor_2)
+print(valor_1 or valor_2)
+print(valor_1 and not valor_2)
+print(not valor_1 or not valor_2)
