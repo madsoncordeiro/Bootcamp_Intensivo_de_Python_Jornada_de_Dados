3510c1f494b6078d9f77922418e3df7f1e89455e
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-03 02:22:08 -0300

    Adicionando arquivo do Exercício 05

---
+++ Aula02/exercicio05.py
@@ -0,0 +1,5 @@
+# 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+
+numero = int(input("Digite um número: "))
+
+print(f"O quadrado de {numero} é igual a {numero * numero}. ")
