97e51b1b6f3aae9554abca22d76718267b703ee8
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-04 02:27:19 -0300

    Incluindo try-except no código para tratar possíveis falhas

--- Aula02/exercicio07.py
+++ Aula02/exercicio07.py
@@ -1,8 +1,11 @@
 # 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
 
-numero1 = float(input("Digite o primeiro número: "))
-numero2 = float(input("Digite o segundo número: "))
-soma = numero1 + numero2
-media = soma / 2
+try:
+    numero1 = float(input("Digite o primeiro número: "))
+    numero2 = float(input("Digite o segundo número: "))
+    soma = numero1 + numero2
+    media = soma / 2
 
-print(f"A média entre os números {numero1} e {numero2} é igual a {round(media, 2)}. ")
+    print(f"A média entre os números {numero1} e {numero2} é igual a {round(media, 2)}. ")
+except ValueError:
+    print("Digite somente um número inteiro formado por algarismos. Não é possível utilizar o número por extenso. ")
