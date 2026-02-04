0e1adb2f3f918b0c69cd58635af22e95dec7ed1f
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-04 02:46:09 -0300

    Incluindo try-except no código para tratar possíveis falhas

--- Aula02/exercicio09.py
+++ Aula02/exercicio09.py
@@ -1,6 +1,9 @@
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
 
-celsius = float(input("Digite a temperatura em graus Celsius: "))
-fahrenheit = float((celsius * 1.8) + 32)
+try:
+    celsius = float(input("Digite a temperatura em graus Celsius: "))
+    fahrenheit = float((celsius * 1.8) + 32)
 
-print(f"A temperatura de {round(celsius, 0)}°C corresponde a {round(fahrenheit, 2)}°F. ")
+    print(f"A temperatura de {round(celsius, 2)}°C corresponde a {round(fahrenheit, 2)}°F. ")
+except ValueError:
+    print("Digite somente um número formado por algarismos. Não é possível utilizar o número por extenso. ")
