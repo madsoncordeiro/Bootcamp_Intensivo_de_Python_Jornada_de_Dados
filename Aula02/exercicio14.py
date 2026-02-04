c205d1ae50f39745fec802fc579302c2fe06f5f9
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-04 07:31:42 -0300

    Incluindo try-except no código para tratar possíveis falhas

--- Aula02/exercicio14.py
+++ Aula02/exercicio14.py
@@ -1,6 +1,9 @@
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 
-data_usuario = input("Digite uma data no formato dd/mm/aaaa: ")
-data_usuario = data_usuario.split("/")
+try:
+    data_usuario = input("Digite uma data no formato dd/mm/aaaa: ")
+    data_usuario = data_usuario.split("/")
 
-print(f"A data que você digitou corresponde ao dia {data_usuario[0]}, mês {data_usuario[1]} e ao ano {data_usuario[2]}. ")
+    print(f"A data que você digitou corresponde ao dia {data_usuario[0]}, mês {data_usuario[1]} e ao ano {data_usuario[2]}. ")
+except IndexError:
+    print("Você digitou a data no formato errado. No caso, você precisa digitar no formato dd/mm/aaaa.")
