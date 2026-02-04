8528d20e87543020902fe48bd28a3f634fc1215b
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-04 01:49:51 -0300

    Incluindo try-except no código para tratar possíveis falhas

--- Aula02/exercicio01.py
+++ Aula02/exercicio01.py
@@ -1,5 +1,7 @@
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
-
-numero1 = int(input("Digite o primeiro número inteiro: "))
-numero2 = int(input("Digite o segundo número inteiro: "))
-print(f"{numero1} + {numero2} = {numero1 + numero2}. ")
+try:
+    numero1 = int(input("Digite o primeiro número inteiro: "))
+    numero2 = int(input("Digite o segundo número inteiro: "))
+    print(f"{numero1} + {numero2} = {numero1 + numero2}. ")
+except ValueError:
+    print(f"Insira somente o(s) algarismo(s) dos números. Não pode inserir o número por extenso ou número decimal. ")
