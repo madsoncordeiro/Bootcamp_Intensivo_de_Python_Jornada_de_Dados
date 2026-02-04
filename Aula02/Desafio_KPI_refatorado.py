a2386cf9d5c0c08ef40143e09450960a8a749808
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-04 19:39:35 -0300

    Adicionando arquivo do desafio proposto pelo professor Luciano Vasconcelos para a Aula02

---
+++ Aula02/Desafio_KPI_refatorado.py
@@ -0,0 +1,21 @@
+"""
+DESAFIO da Aula 01
+Escreva um programa, em Python, que solicita ao usuário para que digite seu nome, valor do seu salário mensal e o valor do bônus que recebeu. O programa deve, então, imprimir uma mensagem saudando o usuário pelo nome, além de informar o valor do salário em comparação com o bônus recebido.
+
+DESAFIO da Aula02
+Refatorar o projeto da aula anterior evitado bugs.
+"""
+
+try:
+    nome_usuario = input("Digite o seu nome: ")
+    salario_usuario = float(input("Digite o seu salário: "))
+    porcentagem_bonus_usuario = float(input("Digite a porcentagem do bônus que você recebeu (digite somente o número): "))
+    
+    if salario_usuario >= 0 and porcentagem_bonus_usuario >= 0:
+        kpi_bonus = 1000 + (salario_usuario * porcentagem_bonus_usuario)
+        print(f"Olá, {nome_usuario}. O seu valor bônus foi de R${kpi_bonus}.")
+    else:
+        print("Você digitou um número negativo para a porcentagem, para o bônus ou para ambos. ")
+
+except ValueError:
+    print("Digite corretamente os valores quando for solicitado (número no lugar de número e nome no lugar de nome). ")
