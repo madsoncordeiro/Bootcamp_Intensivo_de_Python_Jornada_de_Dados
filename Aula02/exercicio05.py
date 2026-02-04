1aa6892331ba95eda2ab5c93d8f3ad15682dd4dd
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-04 02:20:55 -0300

    Incluindo try-except no código para tratar possíveis falhas

--- Aula02/exercicio05.py
+++ Aula02/exercicio05.py
@@ -1,5 +1,8 @@
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
-numero = int(input("Digite um número: "))
+try:
+    numero = int(input("Digite um número: "))
 
-print(f"O quadrado de {numero} é igual a {numero * numero}. ")
+    print(f"O quadrado de {numero} é igual a {numero * numero}. ")
+except ValueError:
+    print("Digite somente um número inteiro formado por algarismos. Não é possível utilizar o número por extenso ou número decimal. ")
