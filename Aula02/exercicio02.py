3e22005234c95513c16837cb73b50077e9ec6d04
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-04 02:03:59 -0300

    Incluindo try-except no código para tratar possíveis falhas

--- Aula02/exercicio02.py
+++ Aula02/exercicio02.py
@@ -1,5 +1,8 @@
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 
-numero = int(input("Digite um número: "))
+try:
+    numero = int(input("Digite um número: "))
 
-print(f"O resto da divisão de {numero} por 5 é igual a {numero % 5}. ")
+    print(f"O resto da divisão de {numero} por 5 é igual a {numero % 5}. ")
+except ValueError:
+    print("Digite somente um número inteiro formado por algarismos. Não é possível utilizar o número por extenso ou número decimal. ")
