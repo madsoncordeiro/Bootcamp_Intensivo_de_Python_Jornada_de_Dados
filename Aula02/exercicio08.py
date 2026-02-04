7efc84dcdb03e889b973428e10a183120853b82c
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-04 02:39:05 -0300

    Incluindo try-except para tratar possíveis falhas

--- Aula02/exercicio08.py
+++ Aula02/exercicio08.py
@@ -1,7 +1,10 @@
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
 
-base = float(input("Digite um número: "))
-expoente = float(input("Digite o expoente: "))
-potencia = base ** expoente
+try:
+    base = float(input("Digite um número: "))
+    expoente = float(input("Digite o expoente: "))
+    potencia = base ** expoente
 
-print(f"O número {base} elevado a {expoente} é igual a {round(potencia, 2)}. ")
+    print(f"O número {base} elevado a {expoente} é igual a {round(potencia, 2)}. ")
+except ValueError:
+    print("Digite somente um número formado por algarismos. Não é possível utilizar o número por extenso. ")
