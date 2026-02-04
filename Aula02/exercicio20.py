951726f1d79ad3ef9c1a2c0ce59ce49be5947f2e
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-04 08:49:11 -0300

    Incluindo try-except no código para tratar possíveis falhas

--- Aula02/exercicio20.py
+++ Aula02/exercicio20.py
@@ -1,9 +1,12 @@
 # 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
 
-numero1 = float(input("Digite o primeiro número: "))
-numero2 = float(input("Digite o segundo número: "))
+try:
+    numero1 = float(input("Digite o primeiro número: "))
+    numero2 = float(input("Digite o segundo número: "))
 
-if numero1 != numero2:
-    print(f"Os números {numero1} e {numero2} são diferentes. ")
-else:
-    print(f"Os números {numero1} e {numero2} são iguais.")
+    if numero1 != numero2:
+        print(f"Os números {numero1} e {numero2} são diferentes. ")
+    else:
+        print(f"Os números {numero1} e {numero2} são iguais.")
+except ValueError:
+    print("Você digitou errado. Não pode ser digitado um número por extenso ou letras. Digite somente números. ")
