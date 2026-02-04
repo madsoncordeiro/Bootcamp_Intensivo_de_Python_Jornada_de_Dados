9804aab99af224b7b499c67dc7e7ce9dd94df187
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-04 02:50:52 -0300

    Incluindo try-except no código para tratar possíveis falhas

--- Aula02/exercicio10.py
+++ Aula02/exercicio10.py
@@ -1,7 +1,10 @@
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
 
-raio = float(input("Digite o valor do raio do círculo: "))
-pi = 3.14159
-area = pi * (raio ** 2)
+try:
+    raio = float(input("Digite o valor do raio do círculo: "))
+    pi = 3.14159
+    area = pi * (raio ** 2)
 
-print(f"O valor da área do círculo cujo raio é {raio:.2f} é igual a {area:.2f}. ")
+    print(f"O valor da área do círculo cujo raio é {raio:.2f} é igual a {area:.2f}. ")
+except ValueError:
+    print("Digite somente um número formado por algarismos. Não é possível utilizar o número por extenso. ")
