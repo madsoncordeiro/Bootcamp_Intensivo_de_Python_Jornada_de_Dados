16c8853d02f963ad870265c6a686f362f2ecb55a
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-03 22:41:07 -0300

    Adicionando arquivo do Exercício 15

---
+++ Aula02/exercicio15.py
@@ -0,0 +1,6 @@
+# 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
+
+string1_usuario = input("Digite o que quiser: ")
+string2_usuario = input("Digite o que quiser novamente: ")
+
+print(f"O que você digitou primeiro foi '{string1_usuario}' e depois foi '{string2_usuario}'. Juntando tudo o que você digitou temos o seguinte: {string1_usuario + " " + string2_usuario}. ")
