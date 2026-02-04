cbbda4c6ad08b6248619654f63b7851936478510
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-04 02:08:47 -0300

    Incluindo try-except no código para tratar possíveis falhas

--- Aula02/exercicio03.py
+++ Aula02/exercicio03.py
@@ -1,6 +1,9 @@
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 
-numero1 = int(input("Digite o primeiro número: "))
-numero2 = int(input("Digite o segundo número: "))
+try:
+    numero1 = int(input("Digite o primeiro número: "))
+    numero2 = int(input("Digite o segundo número: "))
 
-print(f"A multiplicação de {numero1} por {numero2} é igual a {numero1 * numero2}. ")
+    print(f"A multiplicação de {numero1} por {numero2} é igual a {numero1 * numero2}. ")
+except ValueError:
+    print("Digite somente um número inteiro formado por algarismos. Não é possível utilizar o número por extenso ou número decimal. ")
