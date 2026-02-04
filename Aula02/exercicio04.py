30b303725a4c8e2bd6ca68aa56dff68870035213
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-04 02:17:46 -0300

    Incluindo try-except no código para tratar possíveis falhas

--- Aula02/exercicio04.py
+++ Aula02/exercicio04.py
@@ -1,6 +1,11 @@
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 
-numero1 = int(input("Digite o primeiro número inteiro: "))
-numero2 = int(input("Digite o segundo número inteiro: "))
+try:
+    numero1 = int(input("Digite o primeiro número inteiro: "))
+    numero2 = int(input("Digite o segundo número inteiro: "))
 
-print(f"A divisão inteira de {numero1} por {numero2} é igual a {numero1 // numero2}. ")
+    print(f"A divisão inteira de {numero1} por {numero2} é igual a {numero1 // numero2}. ")
+except ValueError:
+    print("Digite somente um número inteiro formado por algarismos. Não é possível utilizar o número por extenso ou número decimal. ")
+except ZeroDivisionError:
+    print("Não é possível dividir um número por zero. Portanto, digite um número diferente de zero quando for solicitado para digitar o segundo número.")
