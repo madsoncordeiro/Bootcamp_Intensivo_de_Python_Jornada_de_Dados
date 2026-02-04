6a57d2630faba1d2875a780cb3404443899b3fb5
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-03 22:25:00 -0300

    Adicionando arquivo do Exercício 12

---
+++ Aula02/exercicio12.py
@@ -0,0 +1,6 @@
+# 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+
+nome_completo_usuario = input("Digite o seu nome completo: ")
+nome_completo_usuario = nome_completo_usuario.lower()
+
+print(nome_completo_usuario)
