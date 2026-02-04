9b726124808e0f4050304cc17413fa76049dfa5f
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-03 22:22:23 -0300

    Adicionando arquivo do Exercício 11

---
+++ Aula02/exercicio11.py
@@ -0,0 +1,6 @@
+# 11. Escreva um programa que receba uma string do usuário e converta para maiúsculas.
+
+string_usuario = input("Digite o que quiser: ")
+string_usuario = string_usuario.upper()
+
+print(string_usuario)
