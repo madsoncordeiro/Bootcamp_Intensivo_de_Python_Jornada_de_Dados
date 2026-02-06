03f53083c6e30901d50df2ad6672b0c07e275ca3
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-06 01:31:41 -0300

    Adicionando arquivo do Exercício 02

---
+++ Aula03/exercicio02.py
@@ -0,0 +1,17 @@
+""" 2. Imagine que você está trabalhando com dados de sensores IoT. Os dados incluem medições de temperatura. Você precisa classificar cada leitura como 'Baixa', 'Normal' ou 'Alta'. Considerando que: 
+- Temperatura < 18°C é 'Baixa'
+- Temperatura >= 18°C e <= 26°C é 'Normal'
+- Temperatura > 26°C é 'Alta'
+"""
+
+try:
+    temperatura = float(input("Digite a temperatura em °C: "))
+
+    if temperatura < 18:
+        print("A temperatura é 'Baixa'. ")
+    elif temperatura >= 18 and temperatura <= 26:
+        print("A temperatura é 'Normal'. ")
+    else:
+        print("A temperatura é 'Alta. ")
+except ValueError:
+    print("Você não digitou um número para indicar o valor da temperatura. ")
