7c05c69291da6b2112c53f25959539b00fc51b11
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-08 22:47:14 -0300

    Adicionando arquivo do Exercício 01

---
+++ Aula04/exercicio01.py
@@ -0,0 +1,6 @@
+# 1. Crie uma lista com os números de 1 a 10 e use um loop para imprimir cada número elevado ao quadrado.
+
+lista: list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for elemento in lista:
+    print(f"{elemento}² = {elemento ** 2}")
