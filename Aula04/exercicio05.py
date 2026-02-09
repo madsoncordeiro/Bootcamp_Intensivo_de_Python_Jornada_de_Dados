65db867babe68d1e718fa59b67d87bedf5978dea
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-02-09 06:49:02 -0300

    Corrigindo resolução da questão de acordo com o que realmente foi solicitado pelo professor

--- Aula04/exercicio05.py
+++ Aula04/exercicio05.py
@@ -1,9 +1,10 @@
 # 5. Dada a lista ["maçã", "banana", "cereja"] e o dicionário {"maçã":0.45, "banana":0.30, "cereja":0.65}, calcule o preço total da lista de compras.
 
-lista:list = list["maçã", "banana", "cereja"]
+lista:list = ["maçã", "banana", "cereja"]
 dicionario: dict = {"maçã":0.45, "banana":0.30, "cereja":0.65}
+preco_total: float = float(0.0)
 
-for preco in dicionario:
-    preco_total: float = float(sum(dicionario.values()))
+for item in lista:
+    preco_total += dicionario[item]
 
 print(f"Preço total: R${preco_total:.2f}.")
